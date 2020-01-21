2db9b85a3e0c49f2ff142f5e65e779eb691b987e
foyez <foyezar@yahoo.com>
2020-01-21 13:45:37 +0600

    add CartItem component & add reselect for redux selector to memoize

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import Button from '../UI/Button/Button';
+import CartItem from '../CartItem/CartItem';
 import './CartDropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      { cartItems.map(cartItem => (
+        <CartItem key={ cartItem.id } item={ cartItem } />
+      )) }
+    </div>
     <Button>GO TO CHECKOUT</Button>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { toggleCart } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/img/shopping-bag.svg';
 import './CartIcon.scss';
 
-const CartIcon = ({ toggleCart }) => (
+const CartIcon = ({ toggleCart, itemCount }) => (
   <div className="cart-icon" onClick={ toggleCart }>
     <ShoppingIcon className='shopping-icon' />
-    <span className="item-count">0</span>
+    <span className="item-count">{ itemCount }</span>
   </div>
 );
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCart: () => dispatch(toggleCart()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
