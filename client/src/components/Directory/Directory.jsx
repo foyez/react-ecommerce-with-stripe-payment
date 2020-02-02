e8e2e356c7b34c9b73509c58bc36e8b273f62d8c
foyez <foyezar@yahoo.com>
2020-02-02 23:19:59 +0600

    add styles for mobile responsive

--- client/src/components/Directory/Directory.jsx
+++ client/src/components/Directory/Directory.jsx
@@ -7,19 +7,13 @@ import { selectSections } from '../../redux/directory/directory.selectors';
 import './Directory.scss';
 import MenuItem from '../MenuItem/MenuItem';
 
-const Directory = ({ sections }) => (
-  <div className='directory-menu'>
-    { sections.map(({ id, ...rest }) => (
-      <MenuItem
-        key={ id }
-        { ...rest }
-      />
-    )) }
-  </div>
-);
+const Directory = ({ sections }) => {
+	console.log(sections);
+	return <div className="directory-menu">{sections.map(({ id, ...rest }) => <MenuItem key={id} {...rest} />)}</div>;
+};
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectSections,
+	sections: selectSections
 });
 
 export default connect(mapStateToProps)(Directory);
--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -10,86 +10,86 @@ import './SignUp.scss';
 // import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 class SignUp extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      displayName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-    };
-  }
+		this.state = {
+			displayName: '',
+			email: '',
+			password: '',
+			confirmPassword: ''
+		};
+	}
 
-  handleSubmit = e => {
-    e.preventDefault();
+	handleSubmit = (e) => {
+		e.preventDefault();
 
-    const { displayName, email, password, confirmPassword } = this.state;
-    const { signUpStart } = this.props;
+		const { displayName, email, password, confirmPassword } = this.state;
+		const { signUpStart } = this.props;
 
-    if (password !== confirmPassword) {
-      alert("passwords don't match");
-      return;
-    }
+		if (password !== confirmPassword) {
+			alert("passwords don't match");
+			return;
+		}
 
-    signUpStart({ displayName, email, password });
-  };
+		signUpStart({ displayName, email, password });
+	};
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+	};
 
-  render() {
-    const { displayName, email, password, confirmPassword } = this.state;
+	render() {
+		const { displayName, email, password, confirmPassword } = this.state;
 
-    return (
-      <div className="sing-up">
-        <h2 className="title">I do not have a account.</h2>
-        <span>Sign up with your email and password</span>
+		return (
+			<div className="sign-up">
+				<h2 className="title">I do not have a account.</h2>
+				<span>Sign up with your email and password</span>
 
-        <form onSubmit={ this.handleSubmit }>
-          <Input
-            type='text'
-            name='displayName'
-            value={ displayName }
-            onChange={ this.handleChange }
-            label='Display Name'
-            required
-          />
-          <Input
-            type='email'
-            name='email'
-            value={ email }
-            onChange={ this.handleChange }
-            label='Email'
-            required
-          />
-          <Input
-            type='password'
-            name='password'
-            value={ password }
-            onChange={ this.handleChange }
-            label='Password'
-            required
-          />
-          <Input
-            type='password'
-            name='confirmPassword'
-            value={ confirmPassword }
-            onChange={ this.handleChange }
-            label='Confirm Password'
-            required
-          />
-          <Button type='submit'>SIGN UP</Button>
-        </form>
-      </div>
-    );
-  }
+				<form onSubmit={this.handleSubmit}>
+					<Input
+						type="text"
+						name="displayName"
+						value={displayName}
+						onChange={this.handleChange}
+						label="Display Name"
+						required
+					/>
+					<Input
+						type="email"
+						name="email"
+						value={email}
+						onChange={this.handleChange}
+						label="Email"
+						required
+					/>
+					<Input
+						type="password"
+						name="password"
+						value={password}
+						onChange={this.handleChange}
+						label="Password"
+						required
+					/>
+					<Input
+						type="password"
+						name="confirmPassword"
+						value={confirmPassword}
+						onChange={this.handleChange}
+						label="Confirm Password"
+						required
+					/>
+					<Button type="submit">SIGN UP</Button>
+				</form>
+			</div>
+		);
+	}
 }
 
-const mapDispatchToProps = dispatch => ({
-  signUpStart: (credentials) => dispatch(signUpStart(credentials)),
+const mapDispatchToProps = (dispatch) => ({
+	signUpStart: (credentials) => dispatch(signUpStart(credentials))
 });
 
 export default connect(null, mapDispatchToProps)(SignUp);
